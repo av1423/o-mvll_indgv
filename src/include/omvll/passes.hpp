12279450159a6a9d8646e207772a5579b30f7f59
av1423 <a.volkov121985@gmail.com>
2025-11-13 00:17:56 +0500

    Update passes.hpp
    oh

--- src/include/omvll/passes.hpp
+++ src/include/omvll/passes.hpp
@@ -15,7 +15,7 @@
 #include "omvll/passes/function-outline/FunctionOutline.hpp"
 #include "omvll/passes/indirect-branch/IndirectBranch.hpp"
 #include "omvll/passes/indirect-call/IndirectCall.hpp"
-#include "omvll/passes/indirect-gv/IndirectGlobalVariable.cpp"
+#include "omvll/passes/indirect-gv/IndirectGlobalVariable.hpp"
 #include "omvll/passes/logger-bind/LoggerBind.hpp"
 #include "omvll/passes/objc-cleaner/ObjCleaner.hpp"
 #include "omvll/passes/opaque-constants/OpaqueConstants.hpp"
