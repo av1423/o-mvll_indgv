e2351dd98499beb18ac4b0a52e6430367c176cc1
av1423 <a.volkov121985@gmail.com>
2025-11-13 00:21:54 +0500

    Update IndirectGlobalVariable.hpp

--- src/include/omvll/passes/indirect-gv/IndirectGlobalVariable.hpp
+++ src/include/omvll/passes/indirect-gv/IndirectGlobalVariable.hpp
@@ -1,8 +1,12 @@
-// IndirectGlobalVariable.hpp
-
 #pragma once
 
+//
+// This file is distributed under the Apache License v2.0. See LICENSE for
+// details.
+//
+
 #include "llvm/IR/PassManager.h"
+#include <memory>
 
 namespace omvll {
 
@@ -10,6 +14,13 @@ namespace omvll {
 struct IndirectGlobalVariable : llvm::PassInfoMixin<IndirectGlobalVariable> {
   /// Run the pass on the given module.
   llvm::PreservedAnalyses run(llvm::Module &M, llvm::ModuleAnalysisManager &MAM);
+
+private:
+  /// Random number generator used for assigning keys to global variables
+  std::unique_ptr<llvm::RandomNumberGenerator> RNG;
+
+  // Optional: helper function to lower constant expressions in a function
+  void lowerConstantExpr(llvm::Function &F);
 };
 
 } // namespace omvll
